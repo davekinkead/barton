393dd1fe433abb2da1e9b03c4697dd726d165474
Dave Kinkead <dave@kinkead.com.au>
2013-01-05 15:11:50 +1000

    Add QLD Federal Senate data

--- lib/barton/data.rb
+++ lib/barton/data.rb
@@ -1,24 +1,83 @@
 #require 'barton/version'
 require 'csv'
+require 'yaml'
 
 module Barton
   module Data
-    class String
-      def unquote
-        self.gsub(/^"|"$/, '')
-      end
-    end
-      
-    def self.csv2yaml(filename)  
-      yaml = "---\n"
-      CSV.foreach( filename, { :headers => true, :header_converters => :symbol } ) do |row|
-        yaml += "- "
-        row.each do |key, val|
-          yaml += "#{key}: #{val}\n  "
+    class << self
+    
+      #   Public: Merges two electorate.yaml files
+      #     
+      #     :source
+      #     :target
+      #     :electorate
+      #
+      #   Examples:
+      #     
+      #     Barton::Data.merge :source => 'raw/staterepscsv.yaml', :target => 'data/qld-federal-house.yaml', :electorate => 'electorate'
+      #
+      #   Returns the merged (target) yaml string
+      def merge(args)
+        source = YAML.load_file args[:source]
+        target = YAML.load_file args[:target]
+        target.each do |et|
+          source.each do |es|
+            if et['name'] == es[args[:electorate]]
+              et['members'] ||= []
+              member = Hash.new
+              es.each do |key,val|
+                member[key] = val
+              end
+              et['members'].push member
+            end
+          end          
         end
-        yaml += "\n"
+        target.to_yaml
       end
-      yaml
+
+
+      #   Public: Converts a CSV file to yaml
+      #     
+      #     :filename
+      #
+      #   Examples:
+      #     
+      #     Barton::Data.csv2yaml 'raw/staterepscsv.csv'
+      #
+      #   Returns a yaml string
+      def csv2yaml(filename)  
+        yaml = "---\n"
+        CSV.foreach( filename, { :headers => true, :header_converters => :symbol } ) do |row|
+          yaml += "- "
+          row.each do |key, val|
+            yaml += "#{key}: #{val}\n  "
+          end
+          yaml += "\n"
+        end
+        yaml
+      end
+
+
+      #   Public: Converts a CSV file to a hash
+      #     
+      #     :filename
+      #
+      #   Examples:
+      #     
+      #     Barton::Data.csv2hash 'raw/staterepscsv.csv'
+      #
+      #   Returns an array of hashes
+      def csv2hash(filename)
+        hashes = Array.new
+        CSV.foreach( filename, { :headers => true, :header_converters => :symbol } ) do |row|
+          hash = Hash.new
+          row.each do |key, val| 
+            hash[key.to_sym] = val
+          end
+          hashes.push hash 
+        end
+        hashes
+      end
     end
   end
 end
--- spec/data_spec.rb
+++ spec/data_spec.rb
@@ -7,7 +7,7 @@ describe Barton::Data do
       proc { Barton::Data.csv2yaml('no_such_file.txt') }.must_raise Errno::ENOENT
     end
     
-    it "should convert each csv line to a yaml list item" do
+    it "should convert each csv row to a yaml list item" do
       yaml = Barton::Data.csv2yaml 'spec/raw/StateRepsCSV.csv'
       #File.open('spec/raw/test.yaml', 'w') { |file| file.write yaml }
       #puts yaml
@@ -17,6 +17,30 @@ describe Barton::Data do
       first = parsed.first.inject({}){|memo,(k,v)| memo[k.to_sym] = v; memo}
       first[:surname].must_equal 'Brodtmann'
       first[:electorate].must_equal 'Canberra'
+    end
+  end
+  
+  describe 'csv2hash' do
+    it "should only accept a csv file" do
+      proc { Barton::Data.csv2hash('no_such_file.txt') }.must_raise Errno::ENOENT
+    end
+    
+    it "should convert each csv row to hash" do
+      results = Barton::Data.csv2hash 'spec/raw/StateRepsCSV.csv'
+      results.size.must_equal 9
+      five = results[4]
+      five.must_be_instance_of Hash
+      five[:surname].must_equal'Alexander'
+      five[:electorate].must_equal 'Bennelong'
+    end
+  end
+  
+  describe "merge" do
+    it "merge 2 yaml files and return a string" do
+      merged = Barton::Data.merge :source => 'raw/qld-la.yaml', :target => 'spec/data/qld-state.yaml', :electorate => 'electorate'
+      parsed = ''
+      proc { parsed = YAML.load merged }.must_be_silent
+  #    p parsed
     end
   end
 end
